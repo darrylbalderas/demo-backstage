56a288cbf8b9b1d2104f5cd969e120658106a543
Darryl Balderas <drrylbalderas@gmail.com>
2024-03-21 23:19:32 -0700

    Upload catalog resource to s3

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,9 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createRouter, createBuiltinActions  } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import {ScmIntegrations} from '@backstage/integration'
+import { createAwsS3CpAction } from '@roadiehq/scaffolder-backend-module-aws';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -9,6 +11,20 @@ export default async function createPlugin(
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+
+
+
+  const integrations = ScmIntegrations.fromConfig(env.config)
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    reader: env.reader,
+    config: env.config
+  });
+
+
+  const actions = [createAwsS3CpAction(), ...builtInActions];
+
 
   return await createRouter({
     logger: env.logger,
@@ -18,5 +34,6 @@ export default async function createPlugin(
     catalogClient,
     identity: env.identity,
     permissions: env.permissions,
+    actions
   });
 }
