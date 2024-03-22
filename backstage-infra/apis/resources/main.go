ccc3ef5f30bdf0b42efdb03a720f64c04228ee67
Darryl Balderas <drrylbalderas@gmail.com>
2024-03-21 22:15:35 -0700

    Create store resource in aws template

--- backstage-infra/apis/resources/main.go
+++ backstage-infra/apis/resources/main.go
@@ -47,5 +47,24 @@ func main() {
 			},
 		})
 	})
+	r.GET("/systems", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"systems": []string{
+				"data-ingestion",
+				"ml-training",
+				"cloud-platform",
+				"analytics",
+			},
+		})
+	})
+	r.GET("/resource-types", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"resource-types": []string{
+				"aws-s3-bucket",
+				"aws-sqs",
+				"aws-rds-cluster",
+			},
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
